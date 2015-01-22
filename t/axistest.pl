f997e77fd4973a0568dea926da5584856f111bb6
Jose Carlos Pereira <jose.cpereira@optimus.pt>
2015-01-22 01:30:13 +0000

    perltidy

--- t/axistest.pl
+++ t/axistest.pl
@@ -4,9 +4,11 @@ use strict;
 use warnings;
 use Pod::WSDL;
 
-my $p = new Pod::WSDL(source => 'My::AxisTest', 
-	location => 'http://localhost/My/AxisTest',
-	pretty => 1,
-	withDocumentation => 1);
-	
+my $p = new Pod::WSDL(
+    'source'            => 'My::AxisTest',
+    'location'          => 'http://localhost/My/AxisTest',
+    'pretty'            => 1,
+    'withDocumentation' => 1,
+);
+
 print $p->WSDL;
